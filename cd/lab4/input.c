c240c90733ea3f373b0259eb449f113cde14a66a
altair-1 <98402213+altair-1@users.noreply.github.com>
2023-08-24 16:13:46 +0530

    Add files via upload

---
+++ cd/lab4/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc) {
+	int a = 12;
+	int b = 14;
+	char e = 'h';
+	int c = a * b;
+	int d = a / b;
+	if (a == 12 && b == 14){
+		printf("12");
+	}
+	if (b != 12){
+		printf("14");
+	}
+}
