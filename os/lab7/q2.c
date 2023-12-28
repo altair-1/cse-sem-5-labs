565e1f78403b6762446e0a137c166d5f6c2b33ad
altair-1 <98402213+altair-1@users.noreply.github.com>
2023-12-28 12:03:08 +0530

    Add files via upload

---
+++ os/lab7/q2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t mutex,wrt;
+int readCount=0;
+
+void* reader(void* arg){
+    do{
+        sem_wait(&mutex);
+        readCount++;
+        if(readCount==1){
+            sem_wait(&wrt);
+        }
+        sem_post(&mutex);
+        printf("reading %d\n",readCount);
+        sleep(1);
+        sem_wait(&mutex);
+        readCount--;
+        if(readCount==0){
+            sem_post(&wrt);
+        }
+        sem_post(&mutex);
+        sleep(1);
+    } while (1);
+    
+}
+
+void* writer(void* arg){
+    do{
+        sem_wait(&wrt);
+        printf("writing %d\n",readCount);
+        sem_post(&wrt);
+        sleep(1);
+    } while (1);
+}
+
+void main(){
+    pthread_t th[3];
+    sem_init(&mutex,0,1);
+    sem_init(&wrt,0,1);
+    pthread_create(&th[0],NULL,reader,NULL);
+    pthread_create(&th[1],NULL,reader,NULL);
+    pthread_create(&th[2],NULL,writer,NULL);
+    pthread_join(th[0],NULL);
+    pthread_join(th[1],NULL);
+    pthread_join(th[2],NULL);
+}
